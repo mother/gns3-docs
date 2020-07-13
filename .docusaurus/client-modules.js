867cc11f97aa06db39685130c4ebaddf20becfb9
Steven Yuen <stevenyuen@Stevens-MacBook-Pro.local>
2020-07-13 00:10:53 -0400

    Update font

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,9 +1,7 @@
 export default [
   require("/Users/stevenyuen/Sites/gns3-docs/node_modules/remark-admonitions/styles/infima.css"),
   require("/Users/stevenyuen/Sites/gns3-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/stevenyuen/Sites/gns3-docs/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
   require("/Users/stevenyuen/Sites/gns3-docs/node_modules/infima/dist/css/default/default.css"),
   require("/Users/stevenyuen/Sites/gns3-docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
   require("/Users/stevenyuen/Sites/gns3-docs/src/css/custom.css"),
-  require("/Users/stevenyuen/Sites/gns3-docs/node_modules/@docusaurus/plugin-google-analytics/src/analytics"),
 ];
