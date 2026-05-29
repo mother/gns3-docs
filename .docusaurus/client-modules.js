e6de60443df3f39ef73322a6c5a940a480164fb7
YueGuobin <yueguobin@outlook.com>
2026-05-29 16:36:54 +0800

    chore: stop tracking .docusaurus build artifacts

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/yueguobin/myCode/GNS3/gns3-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/yueguobin/myCode/GNS3/gns3-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/yueguobin/myCode/GNS3/gns3-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/yueguobin/myCode/GNS3/gns3-docs/src/css/custom.css'),
-];
